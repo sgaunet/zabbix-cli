e05b0fd0b85b20a1a2566a6299f0e4fbd429fbf1
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2024-10-28 21:12:57 +0100

    refactor: Refactor ZabbixFilter struct and add filter options for Zabbix API get method

--- pkg/zabbix/filter.go
+++ pkg/zabbix/filter.go
@@ -1,5 +1,45 @@
 package zabbix
 
-type ZabbixFilter struct {
-	Name []string `json:"name"`
+// Documentation of zabbix API: https://www.zabbix.com/documentation/6.0/en/manual/api/reference_commentary#common-get-method-parameters
+
+// ZabbixFilter struct is used to filter the results from the Zabbix API (get method only)
+// zabbix will return only those results that exactly match the given filter.
+// Accepts an object, where the keys are property names, and the values are either a single value or an array of values to match against.
+type ZabbixFilterGetMethod struct {
+	filter map[string]interface{}
+}
+
+type zabbixFilterGetMethodOption func(*ZabbixFilterGetMethod)
+
+// NewZabbixFilterGetMethod returns a new ZabbixFilterGetMethod
+func NewZabbixFilterGetMethod(options ...zabbixFilterGetMethodOption) *ZabbixFilterGetMethod {
+	z := &ZabbixFilterGetMethod{
+		filter: make(map[string]interface{}),
+	}
+	for _, opt := range options {
+		opt(z)
+	}
+	return z
+}
+
+// AddFilter adds a filter to the ZabbixFilterGetMethod
+func AddFilter(key string, value interface{}) zabbixFilterGetMethodOption {
+	return func(z *ZabbixFilterGetMethod) {
+		z.filter[key] = value
+	}
+}
+
+// GetFilter returns the filter
+func (z *ZabbixFilterGetMethod) GetFilter() map[string]interface{} {
+	return z.filter
+}
+
+// AddFilterName
+func AddFilterName(name string) zabbixFilterGetMethodOption {
+	return AddFilter("name", name)
+}
+
+// AddFilterNames
+func AddFilterNames(names []string) zabbixFilterGetMethodOption {
+	return AddFilter("name", names)
 }
--- pkg/zabbix/template-get.go
+++ pkg/zabbix/template-get.go
@@ -9,22 +9,26 @@ import (
 	"net/http"
 )
 
-const MethodTemplateGet = "template.get"
+// Documentation of zabbix api: https://www.zabbix.com/documentation/6.0/en/manual/api/reference/template/get
 
-// TemplateGetRequest struct is used to get templates from the Zabbix API
-type TemplateGetRequest struct {
-	JSONRPC string `json:"jsonrpc"`
-	Method  string `json:"method"`
-	Params  struct {
-		Output string       `json:"output"`
-		Filter ZabbixFilter `json:"filter"`
-	} `json:"params"`
-	Auth string `json:"auth"`
-	ID   int    `json:"id"`
+const methodTemplateGet = "template.get"
+
+type templateGetRequestParams struct {
+	Output string                 `json:"output"`
+	Filter map[string]interface{} `json:"filter"`
 }
 
-// TemplateGetResponse struct is used to unmarshal the response from the Zabbix API
-type TemplateGetResponse struct {
+// templateGetRequest struct is used to get templates from the Zabbix API
+type templateGetRequest struct {
+	JSONRPC string                   `json:"jsonrpc"`
+	Method  string                   `json:"method"`
+	Params  templateGetRequestParams `json:"params"`
+	Auth    string                   `json:"auth"`
+	ID      int                      `json:"id"`
+}
+
+// templateGetResponse struct is used to unmarshal the response from the Zabbix API
+type templateGetResponse struct {
 	JSONRPC string `json:"jsonrpc"`
 	Result  []struct {
 		TemplateID string `json:"templateID"`
@@ -34,29 +38,22 @@ type TemplateGetResponse struct {
 }
 
 // NewTemplateGetRequest returns a new TemplateGetRequest
-func (z *ZabbixAPI) NewTemplateGetRequest(templatesNames []string) *TemplateGetRequest {
-	return &TemplateGetRequest{
+func (z *ZabbixAPI) GetTemplates(templatesNames []string) (*templateGetResponse, error) {
+	payload := &templateGetRequest{
 		JSONRPC: JSONRPC,
-		Method:  MethodTemplateGet,
-		Params: struct {
-			Output string       `json:"output"`
-			Filter ZabbixFilter `json:"filter"`
-		}{
+		Method:  methodTemplateGet,
+		Params: templateGetRequestParams{
 			Output: "extend",
-			Filter: ZabbixFilter{
-				Name: templatesNames,
-			},
+			Filter: NewZabbixFilterGetMethod(AddFilter("name", templatesNames)).GetFilter(),
 		},
 		Auth: z.Auth(),
-		ID:   1,
+		ID:   generateUniqueID(),
 	}
-}
 
-// GetTemplate returns the templates from the Zabbix API
-func (z *ZabbixAPI) GetTemplate(c *TemplateGetRequest) (*TemplateGetResponse, error) {
-	// initialize auth token
-	c.Auth = z.Auth()
-	postBody, _ := json.Marshal(c)
+	postBody, err := json.Marshal(payload)
+	if err != nil {
+		return nil, fmt.Errorf("cannot marshal data: %w", err)
+	}
 	responseBody := bytes.NewBuffer(postBody)
 	req, err := http.NewRequest(http.MethodPost, z.APIEndpoint, responseBody)
 	if err != nil {
@@ -74,7 +71,7 @@ func (z *ZabbixAPI) GetTemplate(c *TemplateGetRequest) (*TemplateGetResponse, er
 		return nil, fmt.Errorf("cannot read response body: %w", err)
 	}
 
-	var res TemplateGetResponse
+	var res templateGetResponse
 	err = json.Unmarshal(body, &res)
 	if err != nil {
 		return nil, fmt.Errorf("cannot unmarshal response: %w", err)
@@ -82,9 +79,39 @@ func (z *ZabbixAPI) GetTemplate(c *TemplateGetRequest) (*TemplateGetResponse, er
 	return &res, nil
 }
 
+// // GetTemplate returns the templates from the Zabbix API
+// func (z *ZabbixAPI) GetTemplate(c *templateGetRequest) (*templateGetResponse, error) {
+// 	// initialize auth token
+// 	c.Auth = z.Auth()
+// 	postBody, _ := json.Marshal(c)
+// 	responseBody := bytes.NewBuffer(postBody)
+// 	req, err := http.NewRequest(http.MethodPost, z.APIEndpoint, responseBody)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("cannot create request: %w", err)
+// 	}
+// 	req = req.WithContext(context.TODO())
+// 	req.Header.Set("Content-Type", "application/json")
+// 	resp, err := z.client.Do(req)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("cannot do request: %w", err)
+// 	}
+// 	defer resp.Body.Close()
+// 	body, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("cannot read response body: %w", err)
+// 	}
+
+// 	var res templateGetResponse
+// 	err = json.Unmarshal(body, &res)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("cannot unmarshal response: %w", err)
+// 	}
+// 	return &res, nil
+// }
+
 // GetTemplateID returns the ID of the templates
-func (t *TemplateGetResponse) GetTemplateID() []string {
-	var templates []string
+func (t *templateGetResponse) GetTemplateID() []string {
+	var templates []string //nolint: prealloc
 	for _, template := range t.Result {
 		templates = append(templates, template.TemplateID)
 	}
@@ -92,8 +119,8 @@ func (t *TemplateGetResponse) GetTemplateID() []string {
 }
 
 // GetTemplateName returns the name of the templates
-func (t *TemplateGetResponse) GetTemplateName() []string {
-	var templates []string
+func (t *templateGetResponse) GetTemplateName() []string {
+	var templates []string //nolint: prealloc
 	for _, template := range t.Result {
 		templates = append(templates, template.Name)
 	}
