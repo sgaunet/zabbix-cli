3a7335ba5e775d5c6fafe86049fe2713abbe9bf2
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2025-06-02 21:13:48 +0200

    chore: fix linter errors

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,14 @@
+// Package config provides configuration management for the Zabbix CLI application.
 package config
 
-// Config struct holds the configuration for the application
+// Config struct holds the configuration for the application.
 type Config struct {
 	ZabbixEndpoint string `mapstructure:"zabbix_endpoint"`
 	ZabbixUser     string `mapstructure:"zabbix_user"`
 	ZabbixPassword string `mapstructure:"zabbix_password"`
 }
 
-// IsValid checks if the configuration is valid
+// IsValid checks if the configuration is valid.
 func (c *Config) IsValid() bool {
 	if c.ZabbixEndpoint == "" || c.ZabbixUser == "" || c.ZabbixPassword == "" {
 		return false
