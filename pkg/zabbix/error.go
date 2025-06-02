39d5a9b1296a1a729c51e964cded82812cac7718
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2025-06-02 21:17:23 +0200

    chore: fix linter errors

--- pkg/zabbix/error.go
+++ pkg/zabbix/error.go
@@ -2,17 +2,20 @@ package zabbix
 
 import "errors"
 
+// ErrEmptyResult is returned when the result is empty.
 var ErrEmptyResult = errors.New("empty result")
+
+// ErrWrongHTTPCode is returned when the HTTP code is wrong.
 var ErrWrongHTTPCode = errors.New("wrong http code")
 
-// ErrorMsg is the error message returned by the Zabbix API
+// ErrorMsg is the error message returned by the Zabbix API.
 type ErrorMsg struct { //nolint:errname
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 	Data    string `json:"data"`
 }
 
-// Error returns the error message
+// Error returns the error message.
 func (e *ErrorMsg) Error() string {
 	return e.Message + " " + e.Data
 }
